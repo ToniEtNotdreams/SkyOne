a59090b73bf70a145bc6574ce06d1a7050c33b35
SkyOneFr <francissaussice46@gmail.com>
2025-06-29 10:05:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,3 +22,33 @@ function updateIcon() {
   const currentTheme = htmlElement.getAttribute("data-theme");
   themeToggle.textContent = currentTheme === "dark" ? "🌙" : "☀️";
 }
+
+//Scroller
+const messages = [
+  "Rrejoins-nous vite !",
+  "Plugins exclusifs.",
+  "Évènements hebdomadaires et tournois.",
+  "Rejoignez notre Discord pour plus d'infos."
+];
+
+const scroller = document.getElementById("scroller");
+
+let index = 0;
+
+function showMessage() {
+  scroller.textContent = messages[index];
+  scroller.style.transition = "none";
+  scroller.style.transform = "translateX(100%)"; // start à droite
+
+  requestAnimationFrame(() => {
+    scroller.style.transition = "transform 5s linear";
+    scroller.style.transform = "translateX(-100%)"; // glisse à gauche
+  });
+
+  scroller.addEventListener("transitionend", () => {
+    index = (index + 1) % messages.length;
+    showMessage();
+  }, { once: true });
+}
+
+showMessage();
